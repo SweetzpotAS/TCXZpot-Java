e2c081d866f1b7d245b829654ce0dedbb182816c
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-08 15:30:17 +0100

    Make TCXDate implement TCXSerializable

--- lib/src/main/java/com/sweetzpot/tcxzpot/TCXDate.java
+++ lib/src/main/java/com/sweetzpot/tcxzpot/TCXDate.java
@@ -1,9 +1,10 @@
 package com.sweetzpot.tcxzpot;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class TCXDate {
+public class TCXDate implements TCXSerializable {
 
     public static TCXDate tcxDate(int day, int month, int year, int hour, int minute, int second) {
         Calendar instance = Calendar.getInstance();
@@ -13,6 +14,7 @@ public class TCXDate {
         instance.set(Calendar.HOUR_OF_DAY, hour);
         instance.set(Calendar.MINUTE, minute);
         instance.set(Calendar.SECOND, second);
+        instance.set(Calendar.MILLISECOND, 0);
         return new TCXDate(instance.getTime());
     }
 
@@ -20,5 +22,11 @@ public class TCXDate {
 
     public TCXDate(Date date) {
         this.date = date;
+    }
+
+    @Override
+    public void serialize(Serializer serializer) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        serializer.print(format.format(date));
     }
 }
