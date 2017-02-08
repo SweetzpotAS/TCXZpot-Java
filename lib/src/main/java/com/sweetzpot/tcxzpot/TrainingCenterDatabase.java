ae3e35a19264f7d94e7868236d13312cae58f5b0
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-08 12:38:40 +0100

    Create class to represent TCX Training Center Database

---
+++ lib/src/main/java/com/sweetzpot/tcxzpot/TrainingCenterDatabase.java
@@ -0,0 +1,12 @@
+package com.sweetzpot.tcxzpot;
+
+public class TrainingCenterDatabase {
+
+    private final Activities activities;
+    private final AbstractSource author;
+
+    public TrainingCenterDatabase(Activities activities, AbstractSource author) {
+        this.activities = activities;
+        this.author = author;
+    }
+}
