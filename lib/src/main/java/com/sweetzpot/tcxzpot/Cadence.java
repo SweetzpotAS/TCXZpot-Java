bbc95c122bdec17c66bf7cb6621c14e14b1b9e32
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-08 12:19:28 +0100

    Create class to represent TCX Cadence

---
+++ lib/src/main/java/com/sweetzpot/tcxzpot/Cadence.java
@@ -0,0 +1,12 @@
+package com.sweetzpot.tcxzpot;
+
+public class Cadence {
+
+    private final double low;
+    private final double high;
+
+    public Cadence(double low, double high) {
+        this.low = low;
+        this.high = high;
+    }
+}
