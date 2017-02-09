c2b52178289a2bbe30332c3c437a21be87eee644
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-09 17:01:46 +0100

    Create Application builder

--- lib/src/test/java/com/sweetzpot/tcxzpot/ApplicationTest.java
+++ lib/src/test/java/com/sweetzpot/tcxzpot/ApplicationTest.java
@@ -2,6 +2,7 @@ package com.sweetzpot.tcxzpot;
 
 import org.junit.Test;
 
+import static com.sweetzpot.tcxzpot.builders.ApplicationBuilder.application;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -12,12 +13,34 @@ public class ApplicationTest {
         Serializer serializer = mock(Serializer.class);
         Build build = mock(Build.class);
 
-        Application application = new Application("SweetZpot Rowing", build, "en", "123-45678-90");
-        application.serialize(serializer);
+        application("SweetZpot Rowing").withBuild(build)
+                .withLanguageID("en")
+                .withPartNumber("123-45678-90")
+                .build().serialize(serializer);
 
         verify(serializer).print("<Name>SweetZpot Rowing</Name>");
         verify(build).serialize(serializer);
         verify(serializer).print("<LangID>en</LangID>");
         verify(serializer).print("<PartNumber>123-45678-90</PartNumber>");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionIfMissingName() throws Exception {
+        application(null).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionIfMissingBuild() throws Exception {
+        application("SweetZpot Rowing").build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionIfMissingLanguage() throws Exception {
+        application("SweetZpot Rowing").withBuild(mock(Build.class)).build();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throwsExceptionIfMissingPartNumber() throws Exception {
+        application("SweetZpot Rowing").withBuild(mock(Build.class)).withLanguageID("en").build();
     }
 }
