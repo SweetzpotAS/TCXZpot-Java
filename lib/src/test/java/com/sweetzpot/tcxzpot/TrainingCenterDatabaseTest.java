4797d0bb749ea5d04e7b11b0a7f5097ed23427c6
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-02-09 10:52:32 +0100

    Make TrainingCenterDatabase implement TCXSerializable

---
+++ lib/src/test/java/com/sweetzpot/tcxzpot/TrainingCenterDatabaseTest.java
@@ -0,0 +1,49 @@
+package com.sweetzpot.tcxzpot;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+public class TrainingCenterDatabaseTest {
+
+    @Test
+    public void producesCorrectSerialization() throws Exception {
+        Serializer serializer = mock(Serializer.class);
+        Activities activities = mock(Activities.class);
+        Device device = mock(Device.class);
+        when(device.tcxType()).thenCallRealMethod();
+
+        TrainingCenterDatabase trainingCenterDatabase = new TrainingCenterDatabase(activities, device);
+        trainingCenterDatabase.serialize(serializer);
+
+        verify(serializer).print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        verify(serializer).print("<TrainingCenterDatabase\n" +
+                "  xsi:schemaLocation=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd\"\n" +
+                "  xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"\n" +
+                "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
+        verify(activities).serialize(serializer);
+        verify(serializer).print("<Author xsi:type=\"Device_t\">");
+        verify(device).serialize(serializer);
+        verify(serializer).print("</Author>");
+        verify(serializer).print("</TrainingCenterDatabase>");
+    }
+
+    @Test
+    public void producesCorrectSerializationWithMissingArguments() throws Exception {
+        Serializer serializer = mock(Serializer.class);
+
+        TrainingCenterDatabase trainingCenterDatabase = new TrainingCenterDatabase(null, null);
+        trainingCenterDatabase.serialize(serializer);
+
+        verify(serializer).print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        verify(serializer).print("<TrainingCenterDatabase\n" +
+                "  xsi:schemaLocation=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2 http://www.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd\"\n" +
+                "  xmlns=\"http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2\"\n" +
+                "  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
+        verify(serializer).print("</TrainingCenterDatabase>");
+        verifyNoMoreInteractions(serializer);
+    }
+}
